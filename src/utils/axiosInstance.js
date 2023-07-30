ee23c6e9e69fb60442f70d36c29581e926c2eb18
p-v-sai-sri-charan <60253094+p-v-sai-sri-charan@users.noreply.github.com>
2023-07-30 13:48:43 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import Cookie from 'js-cookie';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://sensegrass-server-nnfxeo2uw-sai-charan-au7.vercel.app',
+    baseURL: 'https://sensegrass-serve.vercel.app',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
